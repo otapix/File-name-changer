86c3da2c0a08c4f7b656d6b541e06279b6b7bacc
otapix@gmail.com <otapix@gmail.com>
2020-10-04 16:31:30 +0200

    change extension support

--- README.md
+++ README.md
@@ -1,16 +1,24 @@
 # File-name-changer
-### USAGE:
-    rename.py [-p "path/to/dir/contains/files"] [-f "FILTER"] [-n "name_prefix|start_number|name_suffix"]
-### FILTER:
-    * - all files (default)
-    *.txt - all ".txt" files
-    *[0-9]* - all files contains numbers
-    abc*.txt - all ".txt" files starting with "abc"
-    and more regular expression patterns...
-### example:
-    rename.py -p "c:/my_files" -f "*.txt" -n "new_name_|010|_file"
-### result:
-    new_name_010_file.txt
-    new_name_011_file.txt
-    new_name_012_file.txt
-    new_name_0.._file.txt
+#### USAGE:
+    rename.py [-p "path/to/dir/contains/files"] [-f "FILTER"] [-n "name_prefix|start_number|name_suffix"] [-e "new_extension"]
+#### -p : path to files
+      -p "c:/my_files"
+#### -f : file filter
+      -f "*" - all files (default)
+      -f "*.txt" - all ".txt" files
+      -f "*[0-9]*" - all files contains numbers
+      -f "abc*.txt" - all ".txt" files starting with "abc"
+      -f ... and more regular expression patterns
+#### -n : new name with start number format beatwen "|"
+      -n "my_new_|00100|_file"
+      -n "file |9|"
+      -n "|001|-image"
+#### -e : change extension
+      -e "xxx"
+#### example:
+      rename.py -p "c:/my_files" -f "*.txt" -n "new_name_|010|_file" -e "doc"
+#### result:
+      new_name_010_file.doc
+      new_name_011_file.doc
+      new_name_012_file.doc
+      new_name_0.._file.doc
--- rename.py
+++ rename.py
@@ -6,6 +6,7 @@ args = sys.argv[1:] if len(sys.argv) > 1 else False
 start_number = 1
 new_name = "File_{:03d}{}"
 pattern = '*'
+extension = None
 
 if args:
     if '-n' in args:
@@ -19,6 +20,10 @@ if args:
         index = args.index('-f')
         if len(args) > index + 1:
             pattern = args[index + 1]
+    if '-e' in args:
+        index = args.index('-e')
+        if len(args) > index + 1:
+            extension = args[index + 1]
     if '-p' in args:
         index = args.index('-p')
         if len(args) > index + 1:
@@ -28,7 +33,11 @@ if args:
                 files = glob.glob(os.path.join(directory, pattern))
                 if len(files):
                     for i, f in enumerate(files, start_number):
-                        nn = new_name.format(i, os.path.splitext(f)[1])
+                        if extension:
+                            extension = '.' + extension.replace('.', '')
+                        else:
+                            extension = os.path.splitext(f)[1]
+                        nn = new_name.format(i, extension)
                         if os.path.isfile(nn):
                             print(" File: {} exists.".format(nn))
                         else:
@@ -39,14 +48,25 @@ if args:
             else:
                 print("\n The path '{}' not exists!\n".format(directory))
 
-print(' USAGE: \n    rename.py [-p "path/to/dir/contains/files"] [-f "FILTER"] [-n "name_prefix|start_number|name_suffix"]')
-print(' FILTER: \n    * - all files (default)')
-print('    *.txt - all ".txt" files')
-print('    *[0-9]* - all files contains numbers')
-print('    abc*.txt - all ".txt" files starting with "abc"')
-print('    and more regular expression patterns...')
-print(' example: \n    rename.py -p "c:/my_files" -f "*.txt" -n "new_name_|010|_file"')
-print(' result:  \n    new_name_010_file.txt')
-print('    new_name_011_file.txt')
-print('    new_name_012_file.txt')
-print('    new_name_0.._file.txt')
+print(' USAGE: \n    rename.py [-p "path/to/dir/contains/files"] [-f "FILTER"] [-n "name_prefix|start_number|name_suffix"] [-e "new_extension"]')
+print(' -p : path to files')
+print('      -p "c:/my_files"')
+print(' -f : file filter')
+print('      -f "*" - all files (default)')
+print('      -f "*.txt" - all ".txt" files')
+print('      -f "*[0-9]*" - all files contains numbers')
+print('      -f "abc*.txt" - all ".txt" files starting with "abc"')
+print('      -f ... and more regular expression patterns')
+print(' -n : new name with start number format beatwen "|"')
+print('      -n "my_new_|00100|_file"')
+print('      -n "file |9|"')
+print('      -n "|001|-image"')
+print(' -e : change extension')
+print('      -e "xxx"')
+print(' example:')
+print('      rename.py -p "c:/my_files" -f "*.txt" -n "new_name_|010|_file" -e "doc"')
+print(' result:')
+print('      new_name_010_file.doc')
+print('      new_name_011_file.doc')
+print('      new_name_012_file.doc')
+print('      new_name_0.._file.doc')
